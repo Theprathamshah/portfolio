c67eb96ea50698ed283fa54bd6fe2be7dfd6c739
Pratham Shah <itspratham2911@gmail.com>
2026-08-02 14:14:28 +0530

    feat: :bulb: add projects

--- src/components/ui/ProjectCard.tsx
+++ src/components/ui/ProjectCard.tsx
@@ -68,7 +68,7 @@ export const ProjectCard = ({ project, index }: ProjectCardProps) => {
                 className="p-2.5 rounded-full bg-retro-black/5 dark:bg-white/5 text-retro-gray/80 dark:text-white/70 hover:text-retro-orange hover:bg-retro-black/10 dark:hover:bg-white/10 transition-all border border-retro-black/10 dark:border-white/10 hover:border-retro-orange/30"
                 aria-label="View Source"
               >
-                <GitHubIcon className="w-4.5 h-4.5" />
+                <GitHubIcon className="w-5 h-5" />
               </a>
             )}
             {project.link && (
@@ -79,7 +79,7 @@ export const ProjectCard = ({ project, index }: ProjectCardProps) => {
                 className="p-2.5 rounded-full bg-retro-black/5 dark:bg-white/5 text-retro-gray/80 dark:text-white/70 hover:text-retro-orange hover:bg-retro-black/10 dark:hover:bg-white/10 transition-all border border-retro-black/10 dark:border-white/10 hover:border-retro-orange/30"
                 aria-label="Live Demo"
               >
-                <ExternalLinkIcon className="w-4.5 h-4.5" />
+                <ExternalLinkIcon className="w-5 h-5" />
               </a>
             )}
           </div>
